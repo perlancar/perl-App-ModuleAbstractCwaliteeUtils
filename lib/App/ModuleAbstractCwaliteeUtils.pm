a8e0f7c0b241cb27eeac95d2eaca8897aad2c814
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-07-03 12:08:20 +0700

    Initial commit

---
+++ lib/App/ModuleAbstractCwaliteeUtils.pm
@@ -0,0 +1,18 @@
+package App::ModuleAbstractCwaliteeUtils;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: CLI Utilities related to module Abstract cwalitee
+
+=head1 DESCRIPTION
+
+This distribution includes the following utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Module::Abstract::Cwalitee>
